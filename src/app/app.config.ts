153889b05cb87d22725d41e7fabcf07b6fd81b55
Edison Mayorga <mayorga.sebas100@hotmail.com>
2025-10-22 18:16:50 -0500

    Iniciar proyecto - Autenticacion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,36 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    // Agrega el interceptor de ngrok aquí:
+   
+    provideFirebaseApp(() =>
+      initializeApp({
+          apiKey: "AIzaSyB13Wgz9sZVRoAPsLmLx3TIol4Xbp_XY6M",
+          authDomain: "restaurante-tentaculo.firebaseapp.com",
+          projectId: "restaurante-tentaculo",
+          storageBucket: "restaurante-tentaculo.firebasestorage.app",
+          messagingSenderId: "290468542373",
+          appId: "1:290468542373:web:733a08cf2f032ee0a3da4c",
+          measurementId: "G-XMLS77YJY4"    
+        
+
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
+  
   ]
 };
