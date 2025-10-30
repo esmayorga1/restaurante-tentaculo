b415109d9f7a931416f7f6d640ddf8ca4d40e318
Edison Mayorga <mayorga.sebas100@hotmail.com>
2025-10-29 23:31:48 -0500

    Creando servicio de productos

---
+++ src/app/components/admin/products-add/products-add.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-add',
+  imports: [],
+  templateUrl: './products-add.html',
+  styleUrl: './products-add.css',
+})
+export class ProductsAdd {
+
+}
---
+++ src/app/components/admin/products-edit/products-edit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-edit',
+  imports: [],
+  templateUrl: './products-edit.html',
+  styleUrl: './products-edit.css',
+})
+export class ProductsEdit {
+
+}
